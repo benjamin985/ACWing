b8e87b005d76ff015a706591b07f1d78223325b8
benjamin985 <1870022855@qq.com>
2021-11-17 21:53:19 +0800

    AcWing double pointer

--- Problems/0800.cpp
+++ Problems/0800.cpp
@@ -1,9 +1,26 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
 
-int main() {
-	cout << "you are true" << endl;
-	cout << "study" << endl;
-	cout << "Hello World!\nHappy Everday!\n" << endl;
-	return 0;
+int main()
+{
+    int n, m, x;
+    cin >> n >> m >> x;
+    vector<int> A(n);
+    vector<int> B(m);
+    for (int i = 0; i < n; i++) {
+        cin >> A[i];
+    }
+    for (int j = 0; j < m; j++) {
+        cin >> B[j];
+    }
+    int k = m - 1;
+    for (int i = 0; i < n; i++) {
+        while(k >= 0 && A[i] + B[k] > x) {
+            k--;
+        }
+        if (A[i] + B[k] == x) {
+            cout << i << " " << k << endl;
+        }
+    }
+    return 0;
 }
---
+++ Problems/0801.cpp
@@ -0,0 +1,7 @@
+#include <bits/stdc++.h>
+using namesapace std;
+
+int main() 
+{
+	return 0;
+}
