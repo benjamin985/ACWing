1316a4e5d3d3dbf25510b37e5fe2e1e7ee269268
benjamin985 <1870022855@qq.com>
2021-11-18 14:20:34 +0800

    Bit operation

--- Problems/0801.cpp
+++ Problems/0801.cpp
@@ -1,7 +1,27 @@
-#include <bits/stdc++.h>
-using namesapace std;
+#include<bits/stdc++.h>
+using namespace std;
 
-int main() 
+int main()
 {
-	return 0;
+    int n, cnt = 0;
+    cin >> n;
+    vector<int> res(n);
+    vector<int> ans;
+    for (int i = 0; i < n; i++) {
+      cin >> res[i];
+    }
+    for (int i = 0; i < n; i++) {
+        while (res[i]) {
+            if (res[i] & 1 == 1) cnt++;
+            res[i] = res[i] >> 1;
+            // cout << res[i] << " ";
+        }
+        ans.push_back(cnt);
+        cnt = 0;
+        // cout << endl;
+        
+    }
+    for (auto & x : ans) {
+        cout << x << " ";
+    }
 }
